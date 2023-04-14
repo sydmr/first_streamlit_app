34bcb3c3fc27924d93b0ef15bda8822093914500
sydmr <11133267+sydmr@users.noreply.github.com>
2023-04-14 13:56:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,9 +3,10 @@ import streamlit
 streamlit.title('My Parents New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-boiled Free Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
 
-streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-
-streamlit.text('Hard-boiled Free Range Egg')
