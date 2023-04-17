cbbece2aeca4fbd3d96e83fc06c5ff60c6465dfc
sydmr <11133267+sydmr@users.noreply.github.com>
2023-04-17 14:27:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,15 +35,22 @@ streamlit.header("Fruityvice Fruit Advice!")
 #streamlit.text(fruityvice_response) #just writes data to the screen
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
+
+def get_fruit_choice(this_fruit_choice):
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        return fruityvice_normalized
+
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?') #,'Kiwi')
     if not fruit_choice:
         streamlit.error("Please select a fruit to get information.")
     else:
         # streamlit.write('The user entered ', fruit_choice)
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        streamlit.dataframe(fruityvice_normalized)
+        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        # # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        back_from_function = get_fruityvice_data(fruit_choice)
+        streamlit.dataframe(back_from_function)  
 
 except URLError as e:
     streamlit.error()
