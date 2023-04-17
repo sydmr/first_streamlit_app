ec2ea95d632b1426cea74b988da37afe08a5955d
sydmr <11133267+sydmr@users.noreply.github.com>
2023-04-17 14:09:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 try:
     fruit_choice = streamlit.text_input('What fruit would you like information about?') #,'Kiwi')
-    if not fruit_choice:
+    if not fruit_choice and fruit_choice != '':
         streamlit.write('The user entered ', fruit_choice)
     else:
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
