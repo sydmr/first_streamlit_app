1c9a2595a9f363832ae8ac564b234843267074e3
sydmr <11133267+sydmr@users.noreply.github.com>
2023-04-17 14:08:18 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,16 +36,16 @@ streamlit.header("Fruityvice Fruit Advice!")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?') #,'Kiwi')
-  if not fruit_choice:
-    streamlit.write('The user entered ', fruit_choice)
-  else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    streamlit.dataframe(fruityvice_normalized)
+    fruit_choice = streamlit.text_input('What fruit would you like information about?') #,'Kiwi')
+    if not fruit_choice:
+        streamlit.write('The user entered ', fruit_choice)
+    else:
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+        streamlit.dataframe(fruityvice_normalized)
 
 except URLError as e:
-  streamlit.error()
+    streamlit.error()
 
 ########################### secrets file in the app
 # [snowflake]
